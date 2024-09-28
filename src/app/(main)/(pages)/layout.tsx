98c727a51d002bc0a0864c107f2a80b2a50333b4
Madhav Singh <madhavsingh.relish@gmail.com>
2024-09-28 18:49:30 +0530

    Landing page and base setup

---
+++ src/app/(main)/(pages)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = (props: Props) => {
+  return (
+    <div className="border-l-[1px] border-t-[1px] pb-20 h-screenrounded-l-3xl border-muted-foreground/20 overflow-scroll">
+      {props.children}
+    </div>
+  );
+};
+
+export default Layout;
